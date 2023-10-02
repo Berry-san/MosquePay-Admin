9ae0d9ce03f8897e7241709350013ae0d9d86c67
Abdulmalik Sanusi <sanusiu111@gmail.com>
2023-10-02 16:00:34 +0100

    first

---
+++ build/static/js/4.acd01c9d.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunkmosquepay_adminn=self.webpackChunkmosquepay_adminn||[]).push([[4],{4:function(){sessionStorage.removeItem("email"),sessionStorage.removeItem("username"),sessionStorage.removeItem("user_role"),sessionStorage.removeItem("sheikh_id"),sessionStorage.removeItem("admin_iid"),sessionStorage.removeItem("admin_name"),window.location.href="/login"}}]);
+//# sourceMappingURL=4.acd01c9d.chunk.js.map
